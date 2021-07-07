f753dedad92e3e662303457946cbd91a9a5b007f
Monica Danielle <monicadanielle.estrella.lazarte@apcas.ph.education>
2021-07-07 13:59:39 -0800

    Added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//display
+  echo "<p><center>MONICA DANIELLE E. LAZARTE</center></p>";
+  echo "</br>";
+  echo "<p><center>SIMPLE GRADING SYSTEM</center></p>";
+  echo "</br>";
+  echo "<p><center>BSIT-1</center></p>";
+  echo "</br>";
+  echo '_____________________________________________________________________________________________________________________________________________________________________________';
+//
+$grade=['grade'];
+
+if($grade>=101){
+  echo "<p><center>Course Grade: 105
+  </br>
+  Wrong Input!</center></p>";
+}
+?>
+ 
